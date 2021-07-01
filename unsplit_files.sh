340b1ad7d4baf10230637a9cbc1276bd9fbdcf1b
mgood7123 <smallville7123@gmail.com>
2021-07-02 01:42:06 +1000

    update cmake.yaml

--- unsplit_files.sh
+++ unsplit_files.sh
@@ -1,3 +1,10 @@
-FOLDER=GITHUB_ACTIONS_PREBUILD_OUT_FOLDER
+if [[ $# == 0 ]]
+    then
+        echo "$0 <folder>"
+        echo "folder - the folder to search for split files"
+        exit 1
+fi
+
+FOLDER=$1
 
 find $FOLDER -name \*.SPLIT_MARKER -exec bash -c "DIR=\"\$(dirname {})\"; BASE=\"\$(basename {})\"; cd \$DIR; echo gathering split info for \$BASE; ORIG=\$(cat \${BASE}_NAME); CHMOD=\$(cat \${BASE}_CHMOD); echo gathered split info for \$BASE; echo unsplitting \$ORIG; cat \${ORIG}.SPLIT_FILE* > \$ORIG; chmod \$CHMOD \$ORIG; rm -rf \${ORIG}.SPLIT_*; echo unsplit \$ORIG" \;
