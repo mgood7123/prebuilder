585e611fd47bb416227ad562ea999294884f28af
mgood7123 <smallville7123@gmail.com>
2021-07-02 01:11:48 +1000

    update cmake.yaml

--- split_files_macOS.sh
+++ split_files_macOS.sh
@@ -1,4 +1,4 @@
 FOLDER=GITHUB_ACTIONS_PREBUILD_OUT_FOLDER
-NAME=\*.dylib
+NAME=\*.$1
 
 find $FOLDER -name $NAME -exec bash -c "DIR=\"\$(dirname {})\"; BASE=\"\$(basename {})\"; cd \$DIR; echo creating split info for \$BASE; touch \$BASE.SPLIT_MARKER; echo \$BASE > \$BASE.SPLIT_MARKER_NAME; stat -f \"%OLp\" \$BASE > \$BASE.SPLIT_MARKER_CHMOD; echo created split info for \$BASE; echo splitting \$BASE; split -b 1k \$BASE \$BASE.SPLIT_FILE; rm \$BASE; echo split \$BASE" \;
