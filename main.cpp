5129bc104ab614c54afa0e9a5382b62e297c3124
mgood7123 <smallville7123@gmail.com>
2021-07-02 01:18:36 +1000

    update cmake.yaml

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main() {
     std::cout << "I AM PREBUILT!" << std::endl;
-    return 10;
+    return 11;
 }
--- unsplit_files_macOS.sh
+++
@@ -1,3 +0,0 @@
-FOLDER=GITHUB_ACTIONS_PREBUILD_OUT_FOLDER
-
-find $FOLDER -name \*.SPLIT_MARKER -exec bash -c "DIR=\"\$(dirname {})\"; BASE=\"\$(basename {})\"; cd \$DIR; echo gathering split info for \$BASE; ORIG=\$(cat \${BASE}_NAME); CHMOD=\$(cat \${BASE}_CHMOD); echo gathered split info for \$BASE; echo unsplitting \$ORIG; cat \${ORIG}.SPLIT_FILE* > \$ORIG; chmod \$CHMOD \$ORIG; rm -rf \${ORIG}.SPLIT_*; echo unsplit \$ORIG" \;
