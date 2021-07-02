b1aae93ac9f3de3ff94ed82960560a36539b98bc
mgood7123 <smallville7123@gmail.com>
2021-07-03 06:15:36 +1000

    clean split files, split by 95M

--- split_files.sh
+++ split_files.sh
@@ -4,13 +4,13 @@ if [[ $# < 3 ]]
         echo "folder                    - the folder to search for files"
         echo "extension                 - the file extension to split by"
         echo "command_to_get_chmod_info - a command that outputs the    "
-        echo "                            file's permission flags, eg   "
-        echo "                            output 777 for permissions of "
-        echo "                            read, write, and execute      "
+        echo "                            file's permission flags       "
+        echo "                            eg output 777 for permissions "
+        echo "                            of read, write, and execute   "
         exit 1
 fi
 
 FOLDER=$1
 NAME=\*.$2
 
-find $FOLDER -name $NAME -exec bash -c "DIR=\"\$(dirname {})\"; BASE=\"\$(basename {})\"; cd \$DIR; echo creating split info for \$BASE; touch \$BASE.SPLIT_MARKER; echo \$BASE > \$BASE.SPLIT_MARKER_NAME; $3 \$BASE > \$BASE.SPLIT_MARKER_CHMOD; echo created split info for \$BASE; echo splitting \$BASE; split -b 1k \$BASE \$BASE.SPLIT_FILE; rm \$BASE; echo split \$BASE" \;
+find $FOLDER -name $NAME -exec bash -c "DIR=\"\$(dirname {})\"; BASE=\"\$(basename {})\"; cd \$DIR; echo creating split info for \$BASE; touch \$BASE.SPLIT_MARKER; echo \$BASE > \$BASE.SPLIT_MARKER_NAME; $3 \$BASE > \$BASE.SPLIT_MARKER_CHMOD; echo created split info for \$BASE; echo splitting \$BASE; split -b 95M \$BASE \$BASE.SPLIT_FILE; rm \$BASE; echo split \$BASE" \;
